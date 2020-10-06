e37fd3f783a0e21c9d1f815fa680d31c9eb02b0c
Pramila P <65444226+PramilaPuttu@users.noreply.github.com>
2020-10-06 20:09:49 +0530

    Add files via upload

---
+++ Programming for Everybody (Getting Started with Python)/Week 4/Chapter2-Quiz.py
@@ -0,0 +1,104 @@
+Question 1
+When Python is running in the interactive mode and displaying the chevron prompt (>>>) - what question is Python asking
+
+o	What would you like to do?
+o	What Python script would you like me to run?
+o	What is the next machine language instruction to run?
+o	What is your favourite color?
+
+Answer: What would you like to do?
+
+Question 2
+What will the following program print out:
+>>> x = 15
+>>> x = x + 5
+>>> print(x)
+
+o	20
+o	5
+o	15
+o	"print x"
+o	x + 5
+
+Answer: 20
+
+Question 3
+Python scripts (files) have names that end with:
+
+o	.py
+o	.png
+o	.exe
+o	.doc
+
+Answer: .py
+
+Question 4
+Which of these words is a reserved word in Python ?
+
+o	for
+o	names
+o	pizza
+o	payroll
+
+Answer: for/if/break
+
+Question 5
+What is the proper way to say “good-bye” to Python?
+
+o	while
+o	quit()
+o	#EXIT
+o	// stop
+
+Answer: quit()
+
+Question 6
+Which of the parts of a computer actually executes the program instructions?
+
+o	Input/Output Devices
+o	Main Memory
+o	Central Processing Unit
+o	Secondary Memory
+
+Answer: Central Processing Unit
+
+Question 7
+What is "code" in the context of this course?
+
+o	A way to encrypt data during World War II
+o	A sequence of instructions in a programming language
+o	A set of rules that govern the style of programs
+o	A password we use to unlock Python features
+
+Answer: A sequence of instructions in a programming language
+
+Question 8
+A USB memory stick is an example of which of the following components of computer architecture?
+
+o	Output Device
+o	Main Memory
+o	Secondary Memory
+o	Central Processing Unit
+
+Answer: Secondary Memory
+
+Question 9
+What is the best way to think about a "Syntax Error" while programming?
+
+o	The computer did not understand the statement that you entered
+o	The computer needs to have its software upgraded
+o	The computer has used GPS to find your location and hates everyone from your town
+o	The computer is overheating and just wants you to stop to let it cool down
+
+Answer: The computer did not understand the statement that you entered
+
+Question 10
+Which of the following is not one of the programming patterns covered in Chapter 1?
+
+o	Sequential Steps
+o	Repeated Steps
+o	Conditional Steps
+o	Random steps
+
+Answer: Random steps
+
